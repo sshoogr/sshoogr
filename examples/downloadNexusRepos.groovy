14efac5daf990e78280777e9f76d53c7118966a7
Andrey Adamovich <andrey@aestasit.com>
2013-12-11 18:36:04 +0100

    updated example names added another example

---
+++ examples/downloadNexusRepos.groovy
@@ -0,0 +1,29 @@
+/*
+ * This script connects to remote serverm which hosts Nexus server. It zips a list of repositories 
+ * and downloads zipped file locally.
+ *
+ */
+
+@Grab('com.aestasit.infrastructure.sshoogr:sshoogr:0.9.4')
+import static com.aestasit.ssh.DefaultSsh.*
+
+def folders = [
+  'thirdparty',
+  'releases',
+  'snapshots',
+]
+
+def sshUser = 'root'
+def sshPassword = '123'
+def sshHost = 'build01.server.com'
+
+remoteSession("${sshUser}:${sshPassword}@${sshHost}:22") {
+  folders.each { folder ->
+    exec "zip -9 -q -r /tmp/${folder}.zip /usr/local/sonatype-work/nexus/storage/${folder}"
+    scp {
+      from { remoteFile("/tmp/${folder}.zip") }
+      into { localDir('./repos/') }
+    }
+  }
+}
+
