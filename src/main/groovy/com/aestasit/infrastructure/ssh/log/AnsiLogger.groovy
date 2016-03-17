4d95b738ba9d19fdebb137629d79964d49ca88b2
Andrey Adamovich <andrey@aestasit.com>
2016-03-17 22:16:12 +0200

    fixed license

--- src/main/groovy/com/aestasit/infrastructure/ssh/log/AnsiLogger.groovy
+++ src/main/groovy/com/aestasit/infrastructure/ssh/log/AnsiLogger.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011-2014 Aestas/IT
+ * Copyright (C) 2011-2016 Aestas/IT
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
