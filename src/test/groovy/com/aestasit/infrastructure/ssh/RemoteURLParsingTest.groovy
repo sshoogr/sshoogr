addc9bdbe5e5b20df1dc1600cfca3909298c1dbd
Andrey Adamovich <andrey@aestasit.com>
2017-08-26 17:55:36 +0300

    fixed codenarc warnings

--- src/test/groovy/com/aestasit/infrastructure/ssh/RemoteURLParsingTest.groovy
+++ src/test/groovy/com/aestasit/infrastructure/ssh/RemoteURLParsingTest.groovy
@@ -10,43 +10,43 @@ import org.junit.runners.Parameterized.Parameters
  *
  * @author Jason Darby
  */
-@RunWith(Parameterized.class)
+@RunWith(Parameterized)
 class RemoteURLParsingTest {
 
-    @Parameters
-    static Collection<Object[]> data() {
-        [
-                ['username:password@host:5', 'username', 'password', 'host', 5] as Object[],
-                ['username:p@ssword@host:5', 'username', 'p@ssword', 'host', 5] as Object[],
-                ['username:password@host', 'username', 'password', 'host', 22] as Object[],
-                ['username:p@ssword@host', 'username', 'p@ssword', 'host', 22] as Object[],
-                ['username@host:5', 'username', null, 'host', 5] as Object[],
-                ['host', null, null, 'host', 22] as Object[]
-        ]
-    }
+  @Parameters
+  static Collection<Object[]> data() {
+    [
+        ['username:password@host:5', 'username', 'password', 'host', 5] as Object[],
+        ['username:p@ssword@host:5', 'username', 'p@ssword', 'host', 5] as Object[],
+        ['username:password@host', 'username', 'password', 'host', 22] as Object[],
+        ['username:p@ssword@host', 'username', 'p@ssword', 'host', 22] as Object[],
+        ['username@host:5', 'username', null, 'host', 5] as Object[],
+        ['host', null, null, 'host', 22] as Object[]
+    ]
+  }
 
-    String url
-    String expectedUsername
-    String expectedPassword
-    String expectedHost
-    Integer expectedPort
+  private final String url
+  private final String expectedUsername
+  private final String expectedPassword
+  private final String expectedHost
+  private final Integer expectedPort
 
-    RemoteURLParsingTest(String url, String username, String password, String host, Integer port) {
-        this.url = url
-        this.expectedUsername = username
-        this.expectedPassword = password
-        this.expectedHost = host
-        this.expectedPort = port
-    }
+  RemoteURLParsingTest(String url, String username, String password, String host, Integer port) {
+    this.url = url
+    this.expectedUsername = username
+    this.expectedPassword = password
+    this.expectedHost = host
+    this.expectedPort = port
+  }
 
-    @Test
-    void someTest() {
-        new RemoteURL(url).with {
-            assert user == expectedUsername
-            assert password == expectedPassword
-            assert host == expectedHost
-            assert port == expectedPort
-        }
+  @Test
+  void someTest() {
+    new RemoteURL(url).with {
+      assert user == expectedUsername
+      assert password == expectedPassword
+      assert host == expectedHost
+      assert port == expectedPort
     }
+  }
 
 }
